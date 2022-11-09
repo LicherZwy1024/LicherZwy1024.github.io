fb7e5e4b63ca1735ddd5521a3a91d0a91764bff4
张伟烨 <a_hax@163.com>
2022-11-09 14:00:36 +0800

    dotnet

---
+++ docs/assets/zwy_index.md.f9df7034.js
@@ -0,0 +1,26 @@
+import{_ as s,c as n,o as a,a as l}from"./app.a103cde2.js";const u=JSON.parse('{"title":"Rust \u6362\u6E90","description":"","frontmatter":{},"headers":[{"level":3,"title":"\u521B\u5EFA $HOME/.cargo/config \u6587\u4EF6","slug":"\u521B\u5EFA-home-cargo-config-\u6587\u4EF6"}],"relativePath":"zwy/index.md"}'),o={name:"zwy/index.md"},p=l(`<h1 id="rust-\u6362\u6E90" tabindex="-1">Rust \u6362\u6E90 <a class="header-anchor" href="#rust-\u6362\u6E90" aria-hidden="true">#</a></h1><h3 id="\u521B\u5EFA-home-cargo-config-\u6587\u4EF6" tabindex="-1">\u521B\u5EFA <code>$HOME/.cargo/config</code> \u6587\u4EF6 <a class="header-anchor" href="#\u521B\u5EFA-home-cargo-config-\u6587\u4EF6" aria-hidden="true">#</a></h3><div class="language-cmd"><button class="copy"></button><span class="lang">cmd</span><pre><code><span class="line"><span style="color:#A6ACCD;">[source.crates</span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;">io]</span></span>
+<span class="line"><span style="color:#A6ACCD;">registry </span><span style="color:#89DDFF;">=</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">&quot;</span><span style="color:#C3E88D;">https://github.com/rust-lang/crates.io-index</span><span style="color:#89DDFF;">&quot;</span></span>
+<span class="line"><span style="color:#A6ACCD;"># \u6307\u5B9A\u955C\u50CF</span></span>
+<span class="line"><span style="color:#82AAFF;">replace</span><span style="color:#89DDFF;">-</span><span style="color:#89DDFF;">with</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">=</span><span style="color:#A6ACCD;"> </span><span style="color:#676E95;">&#39;\u955C\u50CF\u6E90\u540D&#39; # \u5982\uFF1Atuna\u3001sjtu\u3001ustc\uFF0C\u6216\u8005 rustcc</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;"># \u6CE8\uFF1A\u4EE5\u4E0B\u6E90\u914D\u7F6E\u4E00\u4E2A\u5373\u53EF\uFF0C\u65E0\u9700\u5168\u90E8</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;"># \u4E2D\u56FD\u79D1\u5B66\u6280\u672F\u5927\u5B66</span></span>
+<span class="line"><span style="color:#A6ACCD;">[source.ustc]</span></span>
+<span class="line"><span style="color:#A6ACCD;">registry </span><span style="color:#89DDFF;">=</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">&quot;</span><span style="color:#C3E88D;">https://mirrors.ustc.edu.cn/crates.io-index</span><span style="color:#89DDFF;">&quot;</span></span>
+<span class="line"><span style="color:#A6ACCD;"># </span><span style="color:#89DDFF;">&gt;&gt;&gt;</span><span style="color:#A6ACCD;"> \u6216\u8005 </span><span style="color:#89DDFF;">&lt;&lt;&lt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">registry </span><span style="color:#89DDFF;">=</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">&quot;</span><span style="color:#C3E88D;">git://mirrors.ustc.edu.cn/crates.io-index</span><span style="color:#89DDFF;">&quot;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;"># \u4E0A\u6D77\u4EA4\u901A\u5927\u5B66</span></span>
+<span class="line"><span style="color:#A6ACCD;">[source.sjtu]</span></span>
+<span class="line"><span style="color:#A6ACCD;">registry </span><span style="color:#89DDFF;">=</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">&quot;</span><span style="color:#C3E88D;">https://mirrors.sjtug.sjtu.edu.cn/git/crates.io-index/</span><span style="color:#89DDFF;">&quot;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;"># \u6E05\u534E\u5927\u5B66</span></span>
+<span class="line"><span style="color:#A6ACCD;">[source.tuna]</span></span>
+<span class="line"><span style="color:#A6ACCD;">registry </span><span style="color:#89DDFF;">=</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">&quot;</span><span style="color:#C3E88D;">https://mirrors.tuna.tsinghua.edu.cn/git/crates.io-index.git</span><span style="color:#89DDFF;">&quot;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;"># rustcc\u793E\u533A</span></span>
+<span class="line"><span style="color:#A6ACCD;">[source.rustcc]</span></span>
+<span class="line"><span style="color:#A6ACCD;">registry </span><span style="color:#89DDFF;">=</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">&quot;</span><span style="color:#C3E88D;">https://code.aliyun.com/rustcc/crates.io-index.git</span><span style="color:#89DDFF;">&quot;</span></span>
+<span class="line"></span>
+<span class="line"></span></code></pre></div>`,3),e=[p];function t(c,r,i,D,y,C){return a(),n("div",null,e)}const F=s(o,[["render",t]]);export{u as __pageData,F as default};
